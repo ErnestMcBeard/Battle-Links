bcce5d2ed645760e7810d491a0a66e14ef78e33d
jomanrones <32491852+jomanrones@users.noreply.github.com>
2018-01-20 18:55:01 -0600

    Player Health
    Added player health to the player prefab

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -7,17 +7,27 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     Rigidbody2D rigidBody2D;
 
+    /// <summary>
+    /// Find components of the player.
+    /// </summary>
     void Start()
     {
         animator = GetComponent<Animator>();
         rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
+    /// <summary>
+    /// Updates every frame.
+    /// </summary>
     void Update()
     {
         UpdateMovement();
     }
 
+    /// <summary>
+    /// Checks the if movement keys are pressed.  If they are, start the respective walking animation and update the velocity of the RigidBody2D.
+    /// If not, set the idle animation per the last movement direction and set the velocity of the RigidBody2D to zero to stop movement.
+    /// </summary>
     void UpdateMovement()
     {
         int currentDirection = animator.GetInteger("direction");
@@ -74,6 +84,12 @@ public class PlayerController : MonoBehaviour
         rigidBody2D.velocity = movementDirection;
     }
 
+    /// <summary>
+    /// Checks to see if the vertical keys are pressed and updates the passed vector without changing the animation.
+    /// We need this because we don't want to set 2 animations when a horizontal movement key and a vertical movement key is pushed.
+    /// </summary>
+    /// <param name="velocity">A Vector to contain the player's movement.</param>
+    /// <returns>An updated vector with the new vertical movement added.</returns>
     Vector2 UpdateVerticalMovement(Vector2 velocity)
     {
         if (Input.GetKey(KeyCode.W))
