e83500f393cd1611429d2ffebbeeb2d662f00973
Elbis <cubatown@gmail.com>
2018-01-21 18:54:17 -0600

    Screen name and class changes
    Changed TitleMenu to Title. Changed TitleManager from prefrab. Placed ignore on Illustrator Files

---
+++ Assets/Scripts/Main Menu/TitleNavigation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class TitleNavigation : MonoBehaviour {
+
+   /// <summary>
+   /// Load scene by name 
+   /// </summary>
+   /// <param name="SceneName"></param>
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+	
+}
