71ea381d307b260d69ec48c1dc279d9cbc8a1571
Elbis <cubatown@gmail.com>
2018-01-21 20:37:07 -0600

    GameController, spawns x number of characters
    Must change GameController to detect #of controllers put in. Also Prefabs do not have movement on creating, why?

---
+++ Assets/Scripts/Player Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public int PlayerCount = 1;
+
+    public GameObject PlayerPrefab;
+
+    	// Use this for initialization
+	void Start () {
+        
+
+            for (int x = 0; x <= PlayerCount; x++)
+            {
+                Instantiate(PlayerPrefab, new Vector2((0+2)*(x%2==0?(-1):(1)), (0+2) * (x % 3 == 0 ? (-1) : (1))), Quaternion.identity);
+            }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
