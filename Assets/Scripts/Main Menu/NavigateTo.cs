4f9a5b28fc99cf6f013362e04acbf775dec1bbaf
Elbis <cubatown@gmail.com>
2018-01-21 18:35:02 -0600

    TitleScreen
    Title screen created with start button navigation to main scene.

---
+++ Assets/Scripts/Main Menu/NavigateTo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class NavigateTo : MonoBehaviour {
+
+   
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+	
+}
